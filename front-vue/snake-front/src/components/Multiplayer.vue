0fa3612ae8ed6c7810c053c96673cc9cb2d6cd7a
leonel <lf.naranjo11@uniandes.edu.co>
2021-04-06 18:36:47 -0500

    obstacles and poisoned fruit added to the game

--- front-vue/snake-front/src/components/Multiplayer.vue
+++ front-vue/snake-front/src/components/Multiplayer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div>
     <ion-phaser v-bind:game.prop="game" v-bind:initialize.prop="initialize" />
   </div>
 </template>
@@ -8,13 +8,65 @@ import Phaser from 'phaser';
 
 export default {
   data() {
+    var image;
+    var text;
+    var timedEvent;
     return {
       initialize: true,
-      game: { type: Phaser.WEBGL, backgroundColor: '#bfcc00' },
-      scene: {
-        preload() {},
-        create() {},
-        update() {},
+      game: {
+        width: 640,
+        height: 480,
+        type: Phaser.CANVAS,
+        backgroundColor: '#eb34e5',
+        scene: {
+          preload() {
+            this.load.setBaseURL('http://labs.phaser.io');
+            this.load.image('ball', 'assets/games/asteroids/asteroid1.png');
+            this.load.image('einstein', 'assets/pics/ra-einstein.png');
+          },
+          create() {
+            image = this.add.image(400, 300, 'einstein');
+
+            text = this.add.text(32, 32);
+            // timedEvent = this.time.addEvent({ delay: 2000, callback: onEvent, callbackScope: this });
+
+            timedEvent = this.time.addEvent({
+              delay: 500,
+              callback: function onEvent() {
+                image.rotation += 0.04;
+              },
+              callbackScope: this,
+              loop: true,
+            });
+            /*
+            var emitter = new Phaser.Events.EventEmitter();
+            var handler = (x, y) => {
+              this.add.image(x, y, 'ball');
+            };
+            emitter.on('addImage', handler, this);
+
+            emitter.emit('addImage', 100, 100);
+            emitter.emit('addImage', 400, 300);
+
+            
+            emitter.emit('addImage', 600, 300);*/
+            this.add.text(16, 16, 'hola', {
+              fontSize: '32px',
+              fill: '#000',
+            });
+          },
+
+          update() {
+            text.setText(
+              'Event.progress: ' +
+                timedEvent
+                  .getProgress()
+                  .toString()
+                  .substr(0, 4)
+            );
+          },
+          render() {},
+        },
       },
     };
   },
--- front-vue/snake-front/src/components/Snake copy.vue
+++ front-vue/snake-front/src/components/Snake copy.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="hello">
+    <div @click="initializeGame" class="flex">
+      <a href="#1" class="btn">Start</a>
+    </div>
     <ion-phaser v-bind:game.prop="game" v-bind:initialize.prop="initialize" />
     <div v-if="finished"><PostScore :score="scoreX" /></div>
   </div>
@@ -14,7 +17,6 @@ export default {
   data() {
     var snake;
     var food;
-    var poison;
     var cursors;
     var UP = 0;
     var DOWN = 1;
@@ -23,11 +25,6 @@ export default {
     var score = 0;
     var scoreText;
     var that = this;
-    var text1;
-    var timedEvent1;
-    var text2;
-    var timedEvent2;
-    var obs;
 
     return {
       finished: false,
@@ -44,61 +41,15 @@ export default {
             this.load.setBaseURL('http://labs.phaser.io');
             this.load.image('body', './assets/games/snake/body.png');
             this.load.image('food', 'assets/games/snake/food.png');
-            this.load.image('poison', 'assets/games/asteroids/asteroid1.png');
-            this.load.image('obstacle', 'assets/games/asteroids/ship.png');
-            this.load.image('tiles', 'assets/tilemaps/tiles/gridtiles.png');
           },
           create() {
             scoreText = this.add.text(16, 16, 'score: 0', {
               fontSize: '32px',
               fill: '#000',
             });
-            var ObstacleSet = new Phaser.Class({
-              initialize: function(scene) {
-                this.grupo = scene.add.group();
-              },
-              addObstacle: function() {
-                var x = Phaser.Math.Between(0, 39);
-                var y = Phaser.Math.Between(0, 29);
-                this.grupo.create(x * 16, y * 16, 'obstacle');
-              },
-            });
-
-            var Poison = new Phaser.Class({
-              Extends: Phaser.GameObjects.Image,
-              initialize: function Poison(scene, x, y) {
-                Phaser.GameObjects.Image.call(this, scene);
-                this.setTexture('poison');
-                this.setPosition(x * 16, y * 16);
-                this.setOrigin(0);
-                this.total = 0;
-                this.alive = true;
-                scene.children.add(this);
-              },
-              eat: function() {
-                var x = Phaser.Math.Between(0, 39);
-                var y = Phaser.Math.Between(0, 29);
-                this.setPosition(x * 16, y * 16);
-              },
-              move: function() {
-                var x = Phaser.Math.Between(0, 39);
-                var y = Phaser.Math.Between(0, 29);
-                this.setPosition(x * 16, y * 16);
-              },
-              disappear: function() {
-                this.alive = false;
-                this.destroy();
-              },
-              apppearAndDisappear: function() {
-                this.alive = !this.alive;
-                if (this.alive) {
-                  this.move();
-                }
-              },
-            });
-
             var Food = new Phaser.Class({
               Extends: Phaser.GameObjects.Image,
+
               initialize: function Food(scene, x, y) {
                 Phaser.GameObjects.Image.call(this, scene);
                 this.setTexture('food');
@@ -203,21 +154,17 @@ export default {
                   this.headPosition.y * 16,
                   1
                 );
-                var hitObs = Phaser.Actions.GetFirst(
-                  obs.grupo.getChildren(),
-                  { x: this.head.x, y: this.head.y },
-                  0
-                );
-
                 var hitBody = Phaser.Actions.GetFirst(
                   this.body.getChildren(),
                   { x: this.head.x, y: this.head.y },
                   1
                 );
-                if (hitBody || hitObs) {
+                if (hitBody) {
                   this.alive = false;
+                  /*this method should call a new component*/
                   that.finished = true;
                   that.scoreX = score;
+
                   return false;
                 } else {
                   this.moveTime = time + this.speed;
@@ -230,43 +177,10 @@ export default {
                   this.tail.y,
                   'body'
                 );
+
                 newPart.setOrigin(0);
               },
-              ungrow: function() {
-                var invaders = this.body.getChildren();
-                if (this.body.getChildren().length >= 3) {
-                  Phaser.Utils.Array.RemoveAt(
-                    invaders,
-                    this.body.getChildren().length - 1,
-                    function(jj) {
-                      jj.destroy();
-                    }
-                  );
-                  Phaser.Utils.Array.RemoveAt(
-                    invaders,
-                    this.body.getChildren().length - 1,
-                    function(jj) {
-                      jj.destroy();
-                    }
-                  );
-                  return true;
-                } else {
-                  this.alive = false;
-                  that.finished = true;
-                  that.scoreX = score;
-                  return false;
-                }
-              },
-              collideWithPoison: function(poison) {
-                if (this.head.x === poison.x && this.head.y === poison.y) {
-                  this.ungrow();
-                  poison.destroy();
 
-                  return true;
-                } else {
-                  return false;
-                }
-              },
               collideWithFood: function(food) {
                 if (this.head.x === food.x && this.head.y === food.y) {
                   this.grow();
@@ -274,59 +188,18 @@ export default {
                   if (this.speed > 20 && food.total % 5 === 0) {
                     this.speed -= 25;
                   }
-                  if (food.total % 10 == 0) {
-                    obs.addObstacle();
-                  }
                   return true;
                 } else {
                   return false;
                 }
               },
             });
+
             snake = new Snake(this, 8, 8);
             food = new Food(this, 4, 3);
-            poison = new Poison(this, 12, 3);
-            text1 = this.add.text(32, 32);
-            text2 = this.add.text(100, 100);
-            obs = new ObstacleSet(this);
-            /*
-            obs = new ObstacleSet();
-            obs.addObstacle();
-*/
-            timedEvent1 = this.time.addEvent({
-              delay: 10000,
-              callback: function apper() {
-                poison = new Poison(this, 12, 3);
-                poison.move();
-              },
-              callbackScope: this,
-              loop: true,
-            });
-            timedEvent2 = this.time.addEvent({
-              delay: 7000,
-              callback: function unapper() {
-                poison.destroy();
-              },
-              callbackScope: this,
-              loop: true,
-            });
             cursors = this.input.keyboard.createCursorKeys();
           },
           update(time) {
-            text1.setText(
-              'Event.progress appear: ' +
-                timedEvent1
-                  .getProgress()
-                  .toString()
-                  .substr(0, 4)
-            );
-            text2.setText(
-              'Event.progress disappear: ' +
-                timedEvent2
-                  .getProgress()
-                  .toString()
-                  .substr(0, 4)
-            );
             if (!snake.alive) {
               return;
             }
@@ -341,13 +214,17 @@ export default {
             }
 
             if (snake.update(time)) {
-              snake.collideWithPoison(poison);
               snake.collideWithFood(food);
             }
           },
         },
       },
     };
+  },
+  methods: {
+    initializeGame() {
+      this.initialize = true;
+    },
   },
 };
 </script>
