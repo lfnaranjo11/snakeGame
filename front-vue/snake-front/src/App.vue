adf99eda7f637170902a24532a4a77c153e0dc88
leonel <lf.naranjo11@uniandes.edu.co>
2021-04-06 12:27:26 -0500

    frontend components reorganization and readme new information

--- front-vue/snake-front/src/App.vue
+++ front-vue/snake-front/src/App.vue
@@ -3,7 +3,7 @@
     <v-card color="basil">
       <v-card-title class="text-center justify-center py-6">
         <h1 class="font-weight-bold display-3 basil--text">
-          BASiL
+          SNake
         </h1>
       </v-card-title>
 
